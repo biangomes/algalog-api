e12f5d85ece32fb1b3f706a7ebdd875d60d0b95c
biangomes <beanascigom@gmail.com>
2023-05-22 22:01:31 -0300

    Implementacao dos codigos HTTP corretos

--- src/main/java/br/com/biangomes/controller/ClienteController.java
+++ src/main/java/br/com/biangomes/controller/ClienteController.java
@@ -2,12 +2,15 @@ package br.com.biangomes.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.biangomes.domain.model.Cliente;
@@ -28,13 +31,14 @@ public class ClienteController {
 		return repo.findAll();
 	}
 
-	@GetMapping("/clientes/nome")
-	public List<Cliente> listarPorNome() {
-		return repo.findByNome("Olívia Cordeiro");
-	}
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = repo.findById(clienteId);
 
-	@GetMapping("/clientes/nome/contem")
-	public List<Cliente> listarPorNomeNaoExato() {
-		return repo.findByNomeContaining("O");
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
